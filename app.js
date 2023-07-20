666ad4dcde28b42c318a4f36d7d0eac3edeef539
KhaledAbuarida <khaledabuarida0@gmail.com>
2023-07-20 21:04:13 +0300

    adding admin & shop routes

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
+//imports
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
+//------ body-parser
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next)=> {
-    res.send('<form method="POST" action="/product"><input type="text" name="title"><button type="submit">Add</button></form>')
-})
+//------ importing Routes
+const adminRoutes = require('./routes/adminRoutes');
+const shopRoutes = require('./routes/shopRoutes');
 
-app.use('/product', (req, res, next)=> {
-    console.log(req.body);
-    // res.send("<h1> Product Page </h1>");
-    res.redirect('/');
-})
+//------ Routes
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req, res, next) => {
-    res.send('<h1> Hello i am Khaled a backend developer </h1>');
-});
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page Not Found! </h1>')
+})
 
 app.listen(5000);
